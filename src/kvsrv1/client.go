dd3aedeed438ce22e07e7ada08ea6a230968482b
fqr <fqr2022@mail.ustc.edu.cn>
2026-08-19 21:27:30 +0800

    lab2 phase 3

--- src/kvsrv1/client.go
+++ src/kvsrv1/client.go
@@ -1,6 +1,8 @@
 package kvsrv
 
 import (
+	"time"
+
 	"6.5840/kvsrv1/rpc"
 	kvtest "6.5840/kvtest1"
 	tester "6.5840/tester1"
@@ -29,12 +31,16 @@ func MakeClerk(clnt *tester.Clnt, server string) kvtest.IKVClerk {
 // arguments. Additionally, reply must be passed as a pointer.
 func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 	// You will have to modify this function.
-	args := rpc.GetArgs{Key: key}
-	reply := rpc.GetReply{}
-	ok := ck.clnt.Call(ck.server, "KVServer.Get", &args, &reply)
-	// TODO:retry method
-	if !ok {
-		return "", 0, rpc.ErrVersion
+	var args rpc.GetArgs
+	var reply rpc.GetReply
+	for {
+		args = rpc.GetArgs{Key: key}
+		reply = rpc.GetReply{}
+		ok := ck.clnt.Call(ck.server, "KVServer.Get", &args, &reply)
+		if ok {
+			break
+		}
+		time.Sleep(100 * time.Millisecond)
 	}
 	return reply.Value, reply.Version, reply.Err
 }
@@ -58,12 +64,21 @@ func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 // arguments. Additionally, reply must be passed as a pointer.
 func (ck *Clerk) Put(key, value string, version rpc.Tversion) rpc.Err {
 	// You will have to modify this function.
-	args := rpc.PutArgs{Key: key, Value: value, Version: version}
-	reply := rpc.PutReply{}
-	ok := ck.clnt.Call(ck.server, "KVServer.Put", &args, &reply)
-	if !ok {
-		// TODO: retry method
-		return rpc.ErrMaybe
+	var args rpc.PutArgs
+	var reply rpc.PutReply
+	retry := false
+	for {
+		args = rpc.PutArgs{Key: key, Value: value, Version: version}
+		reply = rpc.PutReply{}
+		ok := ck.clnt.Call(ck.server, "KVServer.Put", &args, &reply)
+		if ok {
+			if reply.Err == rpc.ErrVersion && retry {
+				return rpc.ErrMaybe
+			}
+			break
+		}
+		retry = true
+		time.Sleep(100 * time.Millisecond)
 	}
 	return reply.Err
 }
