166ec50446b89b023700887e447b58db436b3eb0
fqr <fqr2022@mail.ustc.edu.cn>
2026-08-19 17:38:54 +0800

    lab2: phase 2 complete

--- src/kvsrv1/lock/lock.go
+++ src/kvsrv1/lock/lock.go
@@ -1,8 +1,17 @@
 package lock
 
 import (
-	"6.5840/kvtest1"
+	"time"
+
+	"6.5840/kvsrv1/rpc"
+	kvtest "6.5840/kvtest1"
 )
+
+type LockState struct {
+	name  string
+	owned bool
+	myID  string
+}
 
 type Lock struct {
 	// IKVClerk is a go interface for k/v clerks: the interface hides
@@ -11,6 +20,7 @@ type Lock struct {
 	// MakeLock().
 	ck kvtest.IKVClerk
 	// You may add code here
+	state LockState
 }
 
 // The tester calls MakeLock() and passes in a k/v clerk; your code can
@@ -21,13 +31,62 @@ type Lock struct {
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
 	lk := &Lock{ck: ck}
 	// You may add code here
+	lk.state = LockState{name: l, owned: false, myID: "lockclient" + kvtest.RandValue(8)}
 	return lk
 }
 
 func (lk *Lock) Acquire() {
 	// Your code here
+	for {
+		owner, version, err := lk.ck.Get(lk.state.name)
+		switch err {
+		case rpc.OK:
+			switch owner {
+			case "":
+				err := lk.ck.Put(lk.state.name, lk.state.myID, version)
+				if err == rpc.OK {
+					lk.state.owned = true
+					return
+				}
+			case lk.state.myID:
+				lk.state.owned = true
+				return
+			}
+		case rpc.ErrNoKey:
+			err := lk.ck.Put(lk.state.name, lk.state.myID, 0)
+			if err == rpc.OK {
+				lk.state.owned = true
+				return
+			}
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
 }
 
 func (lk *Lock) Release() {
 	// Your code here
+	if !lk.state.owned {
+		return
+	}
+	for {
+		owner, version, err := lk.ck.Get(lk.state.name)
+		switch err {
+		case rpc.OK:
+			if owner == lk.state.myID {
+				for {
+					err := lk.ck.Put(lk.state.name, "", version)
+					if err == rpc.OK {
+						lk.state.owned = false
+						return
+					}
+				}
+			}
+			lk.state.owned = false
+			return
+		case rpc.ErrNoKey:
+			lk.state.owned = false
+			return
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
 }
